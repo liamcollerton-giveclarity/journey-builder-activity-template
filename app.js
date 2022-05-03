1e0e105e1f188b081f545af816279cd2a08536b7
AzureAD\LiamCollerton <liam.collerton@giveclarity.org>
2022-05-03 20:08:30 +0100

    Remove logger

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var request     = require('request');
 var routes      = require('./routes');
 var activity    = require('./routes/activity');
 
-const logger = require(path.join(__dirname, '.', 'lib', 'logger.js'));
-
 var app = express();
 
 // Configure Express
@@ -41,7 +39,6 @@ app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
 
 http.createServer(app).listen(app.get('port'), function(){
-  logger.info('Express server listening on port ' + app.get('port'));
-
+ 
   console.log('Express server listening on port ' + app.get('port'));
 });
