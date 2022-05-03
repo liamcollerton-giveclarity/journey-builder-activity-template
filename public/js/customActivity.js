baacf51fa1486ef215b0653e8adc17fc488f5056
AzureAD\LiamCollerton <liam.collerton@giveclarity.org>
2022-05-03 19:10:09 +0100

    tweak

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -92,9 +92,9 @@ define([
  //       var postcardURLValue = $('#postcard-url').val();
   //      var postcardTextValue = $('#postcard-text').val();
 
-        payload['arguments'].execute.inArguments = [{
-            "tokens": authTokens
-        }];
+//        payload['arguments'].execute.inArguments = [{
+//            "tokens": authTokens
+//        }];
         
         payload['metaData'].isConfigured = true;
 
